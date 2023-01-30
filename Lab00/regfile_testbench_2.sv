a7da1d10debc07efca584982ac4ef8fd7b41af4d
Thomas Kidd <thomas.kidd@okstate.edu>
2023-01-29 18:46:07 -0600

    updating test bench file to show working ability of Lab 0

--- Lab00/regfile_testbench_2.sv
+++ Lab00/regfile_testbench_2.sv
@@ -36,31 +36,25 @@ regfile dut (clk, we3, ra1, ra2, wa3, wd3, rd1, rd2);
     initial 
         begin      
         
-        #0 ra1 = 5'b00000;
-        #0 ra2 = 5'b00000;
-        #0 we3 = 1'b0;
-        #0 wd3 = 32'h00000000;
-        #5 wd3 = 32'h00000010;
-        
-
         
         
-        // reading data to rd1 and rd2
-        #3 ra1 = 5'b00001;
-        #0 ra2 = 5'b00001;
 
 
         // showing writing data works correctly
-        #12 we3 = 1'b1;
+        #5 we3 = 1'b1;
         #0 wa3 = 5'b00010;
         #0 wd3 = 32'h00000015;
 
-        #2 ra1 = 5'b00010;
-        
+        // writing again
+        #10 we3 = 1'b1;
+        #0 wa3 = 5'b00011;
+        #0 wd3 = 32'h00000016;
 
-        // showing that writing does not take place as enable is off
-        //#5 we3 = 1'b0;
-        //#0 wd3 = 31'h00000014;
+
+        // showing that reading data works combinationally
+        #2 ra1 = 5'b00010;
+        #2 ra2 = 5'b00011;
+    
        
 
         end
